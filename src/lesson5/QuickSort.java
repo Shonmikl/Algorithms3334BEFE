6f3d63ae047501f41b593ef82ad15c238eba9d1c
shonmikl <mikhailegorov676@gmail.com>
2022-10-04 12:33:23 +0300

    Initial commit

---
+++ src/lesson5/QuickSort.java
@@ -0,0 +1,44 @@
+package lesson5;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[22];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println("++++++++++++++++++++++++++++++++++++++");
+        quickSort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+
+    static int partition(int[] array, int begin, int end) {
+
+        int counter = begin;
+        for (int i = begin; i < end; i++) {
+            if (array[i] < array[end]) {
+                int temp = array[counter];
+                array[counter] = array[i];
+                array[i] = temp;
+                counter++;
+            }
+        }
+        int temp = array[end];
+        array[end] = array[counter];
+        array[counter] = temp;
+
+        return counter;
+    }
+
+    public static void quickSort(int[] array, int begin, int end) {
+        if (end <= begin) return;
+        int pivot = partition(array, begin, end);
+        quickSort(array, begin, pivot - 1);
+        quickSort(array, pivot + 1, end);
+    }
+}
