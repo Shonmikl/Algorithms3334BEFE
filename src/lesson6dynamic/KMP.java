79dc7f2dc60237884f3c5aad4d2ea645807aa890
shonmikl <mikhailegorov676@gmail.com>
2022-10-18 12:21:07 +0300

    KMP

--- src/lesson6dynamic/KMP.java
+++ src/lesson6dynamic/KMP.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class KMP {
-    static String source = "ababababbbbbaaaabbbabbabb";
-    static String template = "bbb";
+    static String source = "aabaabaabaabbbbbaaaabbbaabbabb";
+    static String template = "aabaab";
 
     public static void main(String[] args) {
+
         System.out.println(Arrays.toString(search(source, template).toArray()));
+
+        System.out.println(Arrays.toString(prefixFunction(template)));
+
+        System.out.println(Arrays.toString(KMPSearch(source, template).toArray()));
     }
 
     //простой алгоритм
@@ -35,5 +40,73 @@ public class KMP {
             }
         }
         return foundPosition;
+    }
+
+    //отдельный метод нахождения префиксной функции [0 1 0 1 2 3]=={aabaab}
+    static int[] prefixFunction(String template) {
+        //создать массив для функции
+        int[] values = new int[template.length()];
+        /**
+         * если символ в образце совпадает с символом в начале образца
+         * соответсвующую позицию
+         * записываем максимальное значение из того что там уже было
+         * или записываем позицию совпавшего символа внутри образца
+         */
+        for (int i = 1; i < template.length(); i++) {
+            int templatePosition = 0;
+            //ищем внутри себя
+            while (i + templatePosition < template.length()
+            && template.charAt(templatePosition) == template.charAt(i + templatePosition)) {
+                values[i + templatePosition] = Math.max(values[i + templatePosition], templatePosition + 1);
+                templatePosition++;
+            }
+        }
+        return values;
+    }
+
+    public static ArrayList<Integer> KMPSearch(String source, String template) {
+        //массив для нахождения вхождений
+        ArrayList<Integer> found = new ArrayList<>();
+        //вычислить префиксную функцию
+        int[] prefixFunctionArray = prefixFunction(template);
+
+        int sourcePosition = 0;
+        int templatePosition = 0;
+
+        //пока не дошли до конца текста
+        while (sourcePosition < source.length()) {
+            //если символ в шаблоне совпадает с символом в ресурсе
+            if (template.charAt(templatePosition) == source.charAt(sourcePosition)) {
+                //просто двигаемся дальше
+                templatePosition++;
+                sourcePosition++;
+            }
+            //если все символы совпали
+            if (templatePosition == template.length())  {
+                //записываем начало вхождения
+                found.add(sourcePosition - templatePosition);
+                //берем из префиксной функции значение куда
+                //нужно вернуться из последнего значения
+                //(которое совпало)
+                templatePosition = prefixFunctionArray[templatePosition-1];
+                //если не нашли совпадения
+                //...и символ шаблона не совпал с символом ресурса
+
+            } else if(sourcePosition < source.length()
+                    && template.charAt(templatePosition) != source.charAt(sourcePosition)) {
+                //если это не первый символ шаблона
+                if (templatePosition != 0) {
+                    //просто вернуть что находится на соответствующей позиции префиксной функции
+                    //те, если не нашли необходимый элемент то просто вернулись на нужную позицию
+                    //-> вернулись назад в то место с которого можно продолжить поиск
+                    templatePosition = prefixFunctionArray[templatePosition - 1];
+                } else {
+                    //...если все-таки первый символ шаблона
+                    //то просто двигаемся дальше по source
+                    sourcePosition = sourcePosition + 1;
+                }
+            }
+        }
+        return found;
     }
 }
